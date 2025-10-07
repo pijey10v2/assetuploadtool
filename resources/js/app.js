4cbb7d473862a8b94e306bae6fb93e19a2f76e63
pao <paolo.jon@digile.com>
2025-10-07 10:37:48 +0800

    configured app using bootstrap

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+require('./bootstrap');
+
+import Alpine from 'alpinejs';
+
+import './bootstrap';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+window.Alpine = Alpine;
+
+Alpine.start();
