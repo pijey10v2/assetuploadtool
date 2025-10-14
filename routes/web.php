0b5bf608001248573f17434d1324a9253008fc77
pao <paolo.jon@digile.com>
2025-10-14 10:26:33 +0800

    updated laravel version to Laravel Framework 11.46.1

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\UploadToolController;
-
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Here is where you can register web routes for your application.
+| These routes are loaded by the RouteServiceProvider and contain
+| the "web" middleware group. Make something great!
 |
 */
 
@@ -19,22 +19,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Protected dashboard route (only for logged-in users)
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+// Breeze provides built-in auth routes (login, register, etc.)
 require __DIR__.'/auth.php';
 
-Auth::routes();
-
+// Custom logout route (optional, Breeze already includes /logout via POST)
 Route::post('/logout', function () {
     Auth::logout();
     return redirect('/login');
 })->name('logout');
 
+// Your existing controller routes
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/uploadtool', [UploadToolController::class, 'index'])->name('uploadtool');
-Route::post('/uploadtool', [UploadToolController::class, 'store'])->name('uploadtool.store');
-
-
+Route::get('/uploadtool', [UploadToolController::class, 'index'])->middleware(['auth'])->name('uploadtool');
+Route::post('/uploadtool', [UploadToolController::class, 'store'])->middleware(['auth'])->name('uploadtool.store');
